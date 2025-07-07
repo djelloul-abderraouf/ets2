39c4797e496bf9423bb5aad5b8e8192a8f70cc63
abderraouf <keepeer.shop@gmail.com>
2025-07-07 10:27:08 +0000

    Initial commit avec Clerk intégré2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,15 @@ import {
   RedirectToSignIn
 } from '@clerk/clerk-react';
 
-import { UserPlus, Users, FileText, Package, File, Clipboard } from 'react-feather';
+import {
+  UserPlus,
+  Users,
+  FileText,
+  Package,
+  File,
+  Clipboard
+} from 'react-feather';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
@@ -42,11 +50,49 @@ function App() {
 
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto d-flex align-items-center">
+
+              {/* Liens visibles uniquement si connecté */}
               <SignedIn>
                 <li className="nav-item">
+                  <Link className="nav-link d-flex align-items-center" to="/">
+                    <UserPlus size={18} className="me-2" /> Ajouter un Client
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link d-flex align-items-center" to="/clients">
+                    <Users size={18} className="me-2" /> Liste des Clients
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link d-flex align-items-center" to="/create-order">
+                    <FileText size={18} className="me-2" /> Créer un Bon
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link d-flex align-items-center" to="/orders">
+                    <Package size={18} className="me-2" /> Les Bons
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link d-flex align-items-center" to="/devis">
+                    <File size={18} className="me-2" /> Les Devis
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link d-flex align-items-center" to="/facture">
+                    <Clipboard size={18} className="me-2" /> Les Factures
+                  </Link>
+                </li>
+              </SignedIn>
+
+              {/* Bouton utilisateur (profil + déconnexion) */}
+              <SignedIn>
+                <li className="nav-item ms-3">
                   <UserButton afterSignOutUrl="/sign-in" />
                 </li>
               </SignedIn>
+
+              {/* Lien de connexion si non connecté */}
               <SignedOut>
                 <li className="nav-item">
                   <Link className="nav-link" to="/sign-in">Connexion</Link>
@@ -57,10 +103,10 @@ function App() {
         </div>
       </nav>
 
-      {/* Contenu principal */}
+      {/* Contenu de la page */}
       <div className="container py-4">
         <Routes>
-          {/* Page d'accueil : redirige vers /sign-in si non connecté */}
+          {/* Redirection automatique à la connexion */}
           <Route path="/" element={
             <>
               <SignedOut><RedirectToSignIn /></SignedOut>
@@ -72,7 +118,7 @@ function App() {
           <Route path="/sign-in" element={<SignIn routing="path" path="/sign-in" />} />
           <Route path="/sign-up" element={<SignUp routing="path" path="/sign-up" />} />
 
-          {/* Routes protégées : accessibles uniquement si connecté */}
+          {/* Pages sécurisées */}
           <Route path="/clients" element={<SignedIn><ClientList /></SignedIn>} />
           <Route path="/edit/:id" element={<SignedIn><EditClient /></SignedIn>} />
           <Route path="/create-order" element={<SignedIn><CreateOrder /></SignedIn>} />
@@ -80,7 +126,7 @@ function App() {
           <Route path="/devis" element={<SignedIn><DevisList /></SignedIn>} />
           <Route path="/facture" element={<SignedIn><FactureList /></SignedIn>} />
 
-          {/* Routes d'impression non protégées */}
+          {/* Impressions accessibles même sans être connecté */}
           <Route path="/print-order/:orderId" element={<PrintOrder />} />
           <Route path="/print-devis/:devisId" element={<PrintDevis />} />
           <Route path="/print-facture/:factureId" element={<PrintFacture />} />
